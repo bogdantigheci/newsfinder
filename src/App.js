64216e747ef93e24cf3a11d1d5bd33a0b3fcf43e
Your NameBogdan <bogdantigheci@yahoo.com>
2020-07-20 15:27:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [query, setQuery] = useState('react hooks');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const searchInputRef = useRef();
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getNews = () => {
+    setLoading(true);
+    axios
+      .get(`http://hn.algolia.com/api/v1/search?query=${query}`)
+      .then((res) => {
+        setNews(res.data.hits);
+        setLoading(false);
+      })
+      .catch((err) => setError(err));
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getNews();
+  };
+
+  const handleClearSearch = (e) => {
+    setQuery('');
+    searchInputRef.current.focus();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container-fluid mx-auto p-4 bg-blue-lightest  shadow-lg rounded">
+      <h1 className="text-grey-darkest font-thin text-center ">News search</h1>
+      <form onSubmit={handleSearch} className="mb-2 text-center content-center">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          ref={searchInputRef}
+          className="border p-1 rounded"
+        />
+        <button type="submit" className="bg-green rounded m-1 p-1">
+          Search news
+        </button>
+        <button
+          type="button"
+          onClick={handleClearSearch}
+          className="bg-teal p-1 rounded"
         >
-          Learn React
-        </a>
-      </header>
+          Clear search
+        </button>
+      </form>
+      {!loading ? (
+        <ul className="list-reset leading-normal">
+          {news.map((n) => (
+            <li
+              key={n.objectID}
+              className="text-indigo-dark hover:text-indigo-darkest"
+            >
+              <a href={n.url}>{n.title}</a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="font-bold text-orange-dark">Loading news...</div>
+      )}
+      {error ? <div className="text-red font-bold">{error.message}</div> : ''}
     </div>
   );
-}
+};
 
 export default App;
